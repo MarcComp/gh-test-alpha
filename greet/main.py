399701e5d20742f50f29a3f1fdf6badacb7171c7
MarcComp <marcus4moore@gmail.com>
2023-05-11 20:33:35 -0400

    Added version

--- greet/main.py
+++ greet/main.py
@@ -1,5 +1,18 @@
 import click
-   
+from importlib import metadata
+
+def showVersion():
+    vrsn = metadata.version("gh-test-alpha")
+    click.echo(f"Version {vrsn}")
+
 @click.command(no_args_is_help=True, help="Automated greeter")
-def cli():
-    print("Hello World.")
+@click.option("--name","-n", help="Name of person to greet")
+@click.option("--version",is_flag=True, help="Show version")
+def cli(name, version):
+    if (version) :
+        showVersion()
+    else:
+        print(f"Hello to my best friend {name}.")
+
+
+
